ad07ddf4eb56e3218490e1ff895c742a92c1ca50
Christian Rincon <christianrinconalem@gmail.com>
2026-01-21 12:52:45 -0300

    Add helpers
    - Check Directory Existance.
    
    - Move Files into created directory.

--- src/utils/fsHelpers.js
+++ src/utils/fsHelpers.js
@@ -2,21 +2,36 @@ const fs = require('fs');
 const path = require('path');
 
 function getFilesFromDirectory(dirPath) {
-    const items = fs.readdirSync(dirPath);
+  const items = fs.readdirSync(dirPath);
 
-    return items
-        .map(item => {
-            const fullPath = path.join(dirPath, item);
-            const stats = fs.statSync(fullPath);
+  return items
+    .map(item => {
+      const fullPath = path.join(dirPath, item);
+      const stats = fs.statSync(fullPath);
 
-            return {
-                name: item,
-                path: fullPath,
-                isFile: stats.isFile(),
-                isDirectory: stats.isDirectory()
-            };
-        })
-        .filter(item => item.isFile);
+      return {
+        name: item,
+        path: fullPath,
+        isFile: stats.isFile()
+      };
+    })
+    .filter(item => item.isFile);
 }
 
-module.exports = { getFilesFromDirectory };
+function ensureDirectoryExists(dirPath) {
+  if (!fs.existsSync(dirPath)) {
+    fs.mkdirSync(dirPath);
+    return true;
+  }
+  return false;
+}
+
+function moveFile(sourcePath, targetPath) {
+  fs.renameSync(sourcePath, targetPath);
+}
+
+module.exports = {
+  getFilesFromDirectory,
+  ensureDirectoryExists,
+  moveFile
+};
