023667929064954c5249ef247db4ed5a793aefa0
Christian Rincon <christianrinconalem@gmail.com>
2026-01-22 13:04:45 -0300

    Add new file extensions

--- src/rules/byType.js
+++ src/rules/byType.js
@@ -1,11 +1,11 @@
 const RULES_BY_TYPE = {
-  Images: ['.jpg', '.jpeg', '.png', '.gif', '.webp', '.svg'],
-  Documents: ['.pdf', '.doc', '.docx', '.txt', '.md'],
-  Spreadsheets: ['.xls', '.xlsx', '.csv'],
-  Presentations: ['.ppt', '.pptx'],
-  Archives: ['.zip', '.rar', '.7z', '.tar', '.gz'],
-  Audio: ['.mp3', '.wav', '.ogg'],
-  Video: ['.mp4', '.mov', '.avi', '.mkv']
+  Images: ['.jpg', '.jpeg', '.png', '.gif', '.webp', '.svg', '.bmp', '.tiff', '.ico', '.heic', '.psd', '.eps'],
+  Documents: ['.pdf', '.doc', '.docx', '.txt', '.md', '.rtf', '.odt', '.tex', '.wpd', '.epub', '.fb2', '.djvu', '.xps', '.pages'],
+  Spreadsheets: ['.xls', '.xlsx', '.csv', '.ods', '.tsv', '.xlsm', '.xltx', '.xltm', '.xlsb', '.xlam', '.xla', '.xlw', '.xlc', '.xlt'],
+  Presentations: ['.ppt', '.pptx', '.odp', '.key', '.ppsx', '.potx', '.pptm', '.ppsm', '.potm', '.pps', '.pot', '.ppa', '.thmx', '.sldx', '.sldm'],
+  Archives: ['.zip', '.rar', '.7z', '.tar', '.gz', '.bz2', '.xz', '.tgz', '.tbz2'],
+  Audio: ['.mp3', '.wav', '.ogg', '.flac', '.aac', '.m4a'],
+  Video: ['.mp4', '.mov', '.avi', '.mkv', '.webm', '.wmv', '.flv']
 };
 
 const DEFAULT_FOLDER = 'Others';
